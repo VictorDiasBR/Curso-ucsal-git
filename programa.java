1c172f146317b909427218149cf76a2c11c1bef3
Victor <victordiasba@gmail.com>
2017-10-23 20:16:56 -0300

    funcionalidade nova

--- programa.java
+++ programa.java
@@ -1,10 +1,8 @@
 public class programa{
 public static void main(String args[]){
 
-for(i=0;i<11;i++){
+System.out.println("Posição: "+ math.random+" ");
 
-System.out.println("Posição: "+i+"º jogador");
-}
 
 
 }
